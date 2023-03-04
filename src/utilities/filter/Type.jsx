6894121c3642d66ec982bf4f2e24d94ea5451043
oussama ennadafy <oussama.ennadafy@gmail.com>
2023-03-04 21:17:35 +0100

    fix color of of border of the selected type

--- src/utilities/filter/Type.jsx
+++ src/utilities/filter/Type.jsx
@@ -1,10 +1,10 @@
 const Type = ({ img, name, active }) => {
   return (
     <div
-      className={`h-full hover:opacity-100 pt-3 transition-all cursor-pointer after:content-[''] relative after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-gray-300 ${
+      className={`h-full hover:opacity-100 pt-3 transition-all cursor-pointer after:content-[''] relative after:bottom-0 after:left-0 after:w-full after:h-[2px] ${
         active
           ? "opacity-100 after:bg-black after:absolute"
-          : "opacity-50 hover:after:absolute"
+          : "opacity-50 hover:after:absolute after:bg-gray-300"
       }`}
     >
       <img className="w-7 mx-auto mb-1 text-gray" src={img} alt={name} />
