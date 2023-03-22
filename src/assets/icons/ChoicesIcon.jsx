a92eb0d98116890c69a2843757d831dd2980038f
oussama ennadafy <oussama.ennadafy@gmail.com>
2023-03-22 02:14:47 +0000

    add menu in the middle of the night

--- src/assets/icons/ChoicesIcon.jsx
+++ src/assets/icons/ChoicesIcon.jsx
@@ -8,12 +8,12 @@ function ChoicesIcon() {
       <path
         d="M4.5 6.5l1.774 1.774a.25.25 0 00.39-.049L9.5 3.5"
         stroke="#06F"
-        stroke-linecap="round"
+        strokeLinecap="round"
       ></path>
       <path
         d="M16.5 3.5L19 6m0 0l2.5 2.5M19 6l2.5-2.5M19 6l-2.5 2.5"
         stroke="#fff"
-        stroke-linecap="round"
+        strokeLinecap="round"
       ></path>
       <rect
         x="0.5"
--- src/components/Home/Header/UserDetails.jsx
+++ src/components/Home/Header/UserDetails.jsx
@@ -1,20 +1,51 @@
+import { useState } from "react";
 import { earthIcon, menuIcon, userIcon } from "../../../assets/icons";
 
 function UserDetails() {
+  const [displayMenu, setDisplayMenu] = useState(false);
+  const toggleMenu = () => {
+    setDisplayMenu((previousMenuState) => !previousMenuState);
+  };
   return (
-    <div className="flex items-center justify-between">
+    <div className="flex items-center justify-between relative">
       <a className="py-3 px-5 hover:bg-gray-100 rounded-full" href="">
         Airbnb your home
       </a>
       <button className="p-3 hover:bg-gray-100 rounded-full mr-2">
         <img className="w-4" src={earthIcon} alt="earth icon" />
       </button>
-      <button className="flex items-center justify-center gap-3 border border-gray-300 rounded-full p-[6px] hover:shadow-md transition">
+      <button
+        onClick={toggleMenu}
+        className="flex items-center justify-center gap-3 border border-gray-300 rounded-full p-[6px] hover:shadow-md transition"
+      >
         <img className="w-4 m-[2px] ml-1" src={menuIcon} alt="menu icon" />
         <div className="after:content-[''] after:rounded-full relative after:w-[12px] after:h-[12px] after:absolute after:top-[-2px] after:right-[-2px] after:bg-main after:border after:border-white">
           <img className="w-8 h-8" src={userIcon} alt="user icon" />
         </div>
       </button>
+      {displayMenu && (
+        <nav className="absolute top-[115%] shadow-md right-0 rounded-md border border-gray-200 bg-white font-normal py-4 min-w-[80%]">
+          <ul className="flex flex-col border-b border-gray-200 w-full pb-4">
+            <li className="py-3 px-4 hover:bg-gray-50 cursor-pointer">
+              <a href="./">Log in</a>
+            </li>
+            <li className="py-3 px-4 hover:bg-gray-50 cursor-pointer">
+              <a href="./">Sign Up</a>
+            </li>
+          </ul>
+          <ul className="flex flex-col w-full pt-4">
+            <li className="py-3 px-4 hover:bg-gray-50 cursor-pointer">
+              <a href="./">Airbnb your home</a>
+            </li>
+            <li className="py-3 px-4 hover:bg-gray-50 cursor-pointer">
+              <a href="./">host an experience</a>
+            </li>
+            <li className="py-3 px-4 hover:bg-gray-50 cursor-pointer">
+              <a href="./">help</a>
+            </li>
+          </ul>
+        </nav>
+      )}
     </div>
   );
 }
