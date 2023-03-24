5c7373bdde9a5eaa6eb51f86f4f438c2f09c5664
oussama <oussama.ennadafy@gmail.com>
2023-03-24 05:51:21 +0000

    add menu transition

--- src/components/Home/AuthModal/index.jsx
+++ src/components/Home/AuthModal/index.jsx
@@ -4,21 +4,31 @@ import Header from "./Header";
 import Form from "./Form";
 import OtherAuthOptions from "./OtherAuthOptions";
 
-function AuthModal({ setDisplayAuthModal }) {
+function AuthModal({ setDisplayAuthModal, displayAuthModal }) {
   return (
-    <div className="absolute top-0 left-0 w-screen">
+    <>
       <div
         onClick={() => setDisplayAuthModal((previousState) => !previousState)}
-        className="sticky top-0 w-screen h-screen bg-black opacity-50 z-30"
+        className={`fixed top-0 w-screen h-screen bg-black opacity-50 z-30 ${
+          displayAuthModal
+            ? "transition-all duration-500 opacity-100 pointer-events-auto visible"
+            : "opacity-0 pointer-events-none invisible"
+        }`}
       />
-      <section className="sticky top-0 h-full z-30 bg-white w-[560px] overflow-hidden rounded-xl shadow-lg border border-gray-200">
+      <section
+        className={`fixed top-1/2 left-1/2 ${
+          displayAuthModal
+            ? "transition-all duration-500 opacity-100 -translate-y-1/2 pointer-events-auto visible"
+            : "opacity-0 translate-y-full pointer-events-none invisible"
+        } -translate-x-1/2 z-30 bg-white w-[560px] overflow-hidden rounded-xl shadow-lg border border-gray-200`}
+      >
         <Header setDisplayAuthModal={setDisplayAuthModal} />
         <div className="overflow-auto h-[80vh]">
           <Form />
           <OtherAuthOptions />
         </div>
       </section>
-    </div>
+    </>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,16 +18,17 @@ function Home() {
   const hideModal = () => {
     if (displayMenu) toggleMenu();
   };
-  // useEffect(() => {
-  //   if (displayAuthModal)
-  //     document.querySelector("body").classList.add("overflow-y-hidden");
-  //   else document.querySelector("body").classList.remove("overflow-y-hidden");
-  // }, [displayAuthModal]);
+  useEffect(() => {
+    if (displayAuthModal)
+      document.querySelector("body").classList.add("overflow-y-hidden");
+    else document.querySelector("body").classList.remove("overflow-y-hidden");
+  }, [displayAuthModal]);
   return (
     <div onClick={hideModal} className="max-w-screen font-medium">
-      {displayAuthModal && (
-        <AuthModal setDisplayAuthModal={setDisplayAuthModal} />
-      )}
+      <AuthModal
+        displayAuthModal={displayAuthModal}
+        setDisplayAuthModal={setDisplayAuthModal}
+      />
       <AboveHeader />
       <Header
         setDisplayAuthModal={setDisplayAuthModal}
