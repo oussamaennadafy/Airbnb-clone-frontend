f984466647af1ab20d228b9d75515991836db46e
oussama <oussama.ennadafy@gmail.com>
2023-04-06 11:43:55 +0000

    --------------------------------------------------

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Eror404 from "./pages/Error404";
+import Host from "./pages/Host";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="*" element={<Eror404 />} />
+        <Route path="/host" element={<Host />} />
       </Routes>
     </BrowserRouter>
   );
