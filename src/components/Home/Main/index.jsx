d909a064685a085cbb4b3ea467663f1fb48b71be
oussama ennadafy <oussama.ennadafy@gmail.com>
2023-04-28 23:01:27 +0100

    make pagination work

--- src/components/Home/Main/index.jsx
+++ src/components/Home/Main/index.jsx
@@ -6,48 +6,65 @@ import context from "./../../../context";
 export default function Main({ selectedCategory: category }) {
   const [places, setPlaces] = useState([]);
   const [loader, setLoader] = useState(true);
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(0);
+  const [limit, setLimit] = useState(null);
   const mainRef = useRef(null);
-  let limit = null;
-  const getColumns = () => {
-    if (mainRef.current) {
-      const columns = Math.floor(
-        mainRef.current.clientWidth / mainRef.current.firstChild.clientWidth
-      );
-      const ROWS = 5;
-      limit = columns * ROWS;
-    }
-  };
+  const fisrtChildLoaderRef = useRef(null);
+
+  //
+
   useEffect(() => {
-    setLoader(true);
-    console.log(page, limit);
-    console.log(context);
-    fetch(
-      `http://${context.SERVER_IP}:${context.SERVER_PORT}/api/v1/places?page=${page}&limit=${limit}`
-      // to sort use sort=Field
-    )
-      .then((response) => response.json())
-      .then((response) => {
-        // console.log(response.body.places);
-        setPlaces(response.body.places);
-      })
-      .catch((err) => console.log(err))
-      .finally(() => {
-        setLoader(false);
-      });
-  }, [category]);
-  useEffect(getColumns);
-  window.addEventListener("resize", getColumns);
-  if (loader) return <MainLoader />;
+    const loaderObserver = new IntersectionObserver(
+      ([firstEntry]) => {
+        if (firstEntry.intersectionRatio > 0) {
+          setPage((prevPage) => prevPage + 1);
+        }
+      },
+      {
+        root: null,
+      }
+    );
+    loaderObserver.observe(fisrtChildLoaderRef.current);
+  }, []);
+
+  //
+
+  useEffect(() => {
+    if (page) {
+      const main = mainRef.current;
+      const columns = Math.floor(
+        main.clientWidth / main.firstChild.clientWidth
+      );
+      const rows = 2;
+      const limit = columns * rows;
+      setLimit(limit);
+      fetch(
+        `http://${context.SERVER_IP}:${context.SERVER_PORT}/api/v1/places?page=${page}&limit=${limit}`
+      )
+        .then((response) => response.json())
+        .then((response) => {
+          if (response.result === 0) setLoader(false);
+          else
+            setPlaces((prevPlaces) => [...prevPlaces, ...response.body.places]);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [category, page]);
+
+  //
+
   return (
-    <main
-      id="Main"
-      ref={mainRef}
-      className="min-h-screen max-w-screen py-8 px-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6 gap-x-5 gap-y-10 mt-1"
-    >
-      {places.map((place) => (
-        <Item place={place} key={place.title} />
-      ))}
-    </main>
+    <>
+      <main
+        id="Main"
+        ref={mainRef}
+        className="min-h-screen max-w-screen py-8 px-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6 gap-x-5 gap-y-10 mt-1"
+      >
+        {places?.map((place) => (
+          <Item place={place} key={place.title} />
+        ))}
+        {loader ? <MainLoader ref={fisrtChildLoaderRef} limit={limit} /> : null}
+      </main>
+    </>
   );
 }
