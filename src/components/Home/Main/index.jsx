121e8769ce9a5af080a0c5f838db72dbda18faaf
oussama ennadafy <oussama.ennadafy@gmail.com>
2023-05-10 15:14:06 +0100

    fix main page

--- src/components/Home/Main/index.jsx
+++ src/components/Home/Main/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, useCallback } from "react";
 import Item from "./Item";
 import MainLoader from "./../../../utilities/LoadingTmplates/MainLoader";
 import context from "./../../../context";
@@ -7,38 +7,78 @@ export default function Main({ selectedCategory: category }) {
   const [places, setPlaces] = useState([]);
   const [loader, setLoader] = useState(true);
   const [page, setPage] = useState(1);
+
   const mainRef = useRef(null);
-  let limit = null;
-  const getColumns = () => {
-    if (mainRef.current) {
-      const columns = Math.floor(
-        mainRef.current.clientWidth / mainRef.current.firstChild.clientWidth
+  const firstLoadingFigureRef = useRef(null);
+
+  const getLimit = useCallback(() => {
+    const columns = Math.floor(
+      mainRef?.current?.clientWidth / mainRef?.current?.firstChild?.clientWidth
+    );
+    const ROWS = 5;
+    const limit = columns * ROWS;
+    return limit;
+  }, []);
+
+  useEffect(() => {
+    if (firstLoadingFigureRef) {
+      const LoaderObserverCallback = ([firstEntry], observer) => {
+        if (
+          firstEntry.intersectionRatio > 0 &&
+          firstEntry.intersectionRatio < 1
+        ) {
+          setPage((prevPage) => prevPage + 1);
+        }
+      };
+      const LoaderObserverOptions = {
+        root: null,
+      };
+      const LoaderObserver = new IntersectionObserver(
+        LoaderObserverCallback,
+        LoaderObserverOptions
       );
-      const ROWS = 5;
-      limit = columns * ROWS;
+      LoaderObserver.observe(firstLoadingFigureRef.current);
     }
-  };
+  }, []);
+
   useEffect(() => {
     setLoader(true);
-    console.log(page, limit);
-    console.log(context);
+    const limit = getLimit();
+    console.log(`fetch page ${page}, with limit of ${limit} items`);
+    const fields =
+      "_id,images,location,ratingAverage,viewedTimesLastWeek,price,from,to";
     fetch(
-      `http://${context.SERVER_IP}:${context.SERVER_PORT}/api/v1/places?page=${page}&limit=${limit}`
+      `http://${context.SERVER_IP}:${context.SERVER_PORT}/api/v1/places?fields=${fields}&page=${page}&limit=${limit}&category=${category}`
       // to sort use sort=Field
     )
       .then((response) => response.json())
       .then((response) => {
-        // console.log(response.body.places);
-        setPlaces(response.body.places);
+        if (response.body.places.length === 0) setLoader(false);
+        else
+          setPlaces((prevPlaces) => [...prevPlaces, ...response.body.places]);
       })
-      .catch((err) => console.log(err))
-      .finally(() => {
-        setLoader(false);
-      });
+      .catch((err) => console.log(err));
+  }, [page]);
+
+  useEffect(() => {
+    setLoader(true);
+    setPlaces([]);
+    setPage(1);
+    const limit = getLimit();
+    console.log(`fetch page ${page}, with limit of ${limit} items`);
+    const fields =
+      "_id,images,location,ratingAverage,viewedTimesLastWeek,price,from,to";
+    fetch(
+      `http://${context.SERVER_IP}:${context.SERVER_PORT}/api/v1/places?fields=${fields}&page=${page}&limit=${limit}&category=${category}`
+      // to sort use sort=Field
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.body.places.length === 0) setLoader(false);
+        else setPlaces(response.body.places);
+      })
+      .catch((err) => console.log(err));
   }, [category]);
-  useEffect(getColumns);
-  window.addEventListener("resize", getColumns);
-  if (loader) return <MainLoader />;
   return (
     <main
       id="Main"
@@ -46,8 +86,9 @@ export default function Main({ selectedCategory: category }) {
       className="min-h-screen max-w-screen py-8 px-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6 gap-x-5 gap-y-10 mt-1"
     >
       {places.map((place) => (
-        <Item place={place} key={place.title} />
+        <Item place={place} key={place._id} />
       ))}
+      {loader ? <MainLoader limit={10} ref={firstLoadingFigureRef} /> : null}
     </main>
   );
 }
