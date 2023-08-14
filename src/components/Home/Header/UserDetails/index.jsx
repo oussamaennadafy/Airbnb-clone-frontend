f84824064a15cbd12b05a18d86ad46effd14d9f2
oussama ennadafy <oussama.ennadafy@gmail.com>
2023-08-14 13:51:33 +0100

    add overlay

--- src/components/Home/Header/UserDetails/index.jsx
+++ src/components/Home/Header/UserDetails/index.jsx
@@ -15,62 +15,77 @@ const UserDetails = ({ setDisplayAuthModal }) => {
   return (
     <AuthContext.Consumer>
       {(AuthContext) => (
-        <div className="flex items-center justify-between relative">
-          <Link to="/host" className="py-3 px-5 hover:bg-gray-100 rounded-full">
-            Airbnb your home
-          </Link>
-          <button className="p-3 hover:bg-gray-100 rounded-full mr-2">
-            <img className="w-4" src={earthIcon} alt="earth icon" />
-          </button>
-          <button
-            onClick={(e) => {
-              e.stopPropagation();
-              toggleMenu();
-            }}
-            className="flex items-center justify-center gap-3 border border-gray-300 rounded-full p-[6px] hover:shadow-md transition"
-          >
-            <img className="w-4 m-[2px] ml-1" src={menuIcon} alt="menu icon" />
-            <div className="after:content-[''] after:rounded-full relative after:w-[12px] after:h-[12px] after:absolute after:top-[-2px] after:right-[-2px] after:bg-main after:border after:border-white">
-              <img className="w-8 h-8" src={userIcon} alt="user icon" />
-            </div>
-          </button>
+        <>
           {displayMenu && (
-            <nav
+            <div
+              onClick={toggleMenu}
+              className="absolute top-0 left-0 z-40 w-screen h-screen"
+            ></div>
+          )}
+          <div className="flex items-center justify-between relative">
+            <Link
+              to="/host"
+              className="py-3 px-5 hover:bg-gray-100 rounded-full"
+            >
+              Airbnb your home
+            </Link>
+            <button className="p-3 hover:bg-gray-100 rounded-full mr-2">
+              <img className="w-4" src={earthIcon} alt="earth icon" />
+            </button>
+            <button
               onClick={(e) => {
                 e.stopPropagation();
+                toggleMenu();
               }}
-              className="absolute transition top-[140%] moveUp shadow-md right-0 rounded-md border border-gray-200 bg-white font-normal py-4 min-w-[80%]"
+              className="flex items-center justify-center gap-3 border border-gray-300 rounded-full p-[6px] hover:shadow-md transition"
             >
-              {AuthContext.isLoggedIn ? null : (
-                <ul className="flex flex-col border-b border-gray-200 w-full pb-4">
-                  <li
-                    onClick={showAuthModal}
-                    className="py-3 px-4 hover:bg-gray-50 cursor-pointer font-medium"
-                  >
-                    <a>Log in</a>
+              <img
+                className="w-4 m-[2px] ml-1"
+                src={menuIcon}
+                alt="menu icon"
+              />
+              <div className="after:content-[''] after:rounded-full relative after:w-[12px] after:h-[12px] after:absolute after:top-[-2px] after:right-[-2px] after:bg-main after:border after:border-white">
+                <img className="w-8 h-8" src={userIcon} alt="user icon" />
+              </div>
+            </button>
+            {displayMenu && (
+              <nav
+                onClick={(e) => {
+                  e.stopPropagation();
+                }}
+                className="absolute transition top-[140%] moveUp shadow-md right-0 rounded-md border border-gray-200 bg-white font-normal py-4 min-w-[80%] z-50"
+              >
+                {AuthContext.isLoggedIn ? null : (
+                  <ul className="flex flex-col border-b border-gray-200 w-full pb-4">
+                    <li
+                      onClick={showAuthModal}
+                      className="py-3 px-4 hover:bg-gray-50 cursor-pointer font-medium"
+                    >
+                      <a>Log in</a>
+                    </li>
+                    <li
+                      onClick={showAuthModal}
+                      className="py-3 px-4 hover:bg-gray-50 cursor-pointer"
+                    >
+                      <a>Sign Up</a>
+                    </li>
+                  </ul>
+                )}
+                <ul className="flex flex-col w-full pt-4">
+                  <li className="py-3 px-4 hover:bg-gray-50 cursor-pointer">
+                    <Link to="/host">Airbnb your home</Link>
                   </li>
-                  <li
-                    onClick={showAuthModal}
-                    className="py-3 px-4 hover:bg-gray-50 cursor-pointer"
-                  >
-                    <a>Sign Up</a>
+                  <li className="py-3 px-4 hover:bg-gray-50 cursor-pointer">
+                    <a href="./">host an experience</a>
+                  </li>
+                  <li className="py-3 px-4 hover:bg-gray-50 cursor-pointer">
+                    <a href="./">help</a>
                   </li>
                 </ul>
-              )}
-              <ul className="flex flex-col w-full pt-4">
-                <li className="py-3 px-4 hover:bg-gray-50 cursor-pointer">
-                  <Link to="/host">Airbnb your home</Link>
-                </li>
-                <li className="py-3 px-4 hover:bg-gray-50 cursor-pointer">
-                  <a href="./">host an experience</a>
-                </li>
-                <li className="py-3 px-4 hover:bg-gray-50 cursor-pointer">
-                  <a href="./">help</a>
-                </li>
-              </ul>
-            </nav>
-          )}
-        </div>
+              </nav>
+            )}
+          </div>
+        </>
       )}
     </AuthContext.Consumer>
   );
